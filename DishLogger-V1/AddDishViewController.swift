9a0993f1c8c4be1d5bc6597f434908d43d10c9fa
Gerdje Hung-chu <gerdje.hc@gmail.com>
2017-10-27 15:50:46 +0200

    Second Commit vdab

---
+++ DishLogger-V1/AddDishViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddDishViewController.swift
+//  DishLogger-V1
+//
+//  Created by vdab cursist on 27/10/2017.
+//  Copyright © 2017 Gerd Hung-chu. All rights reserved.
+//
+
+import UIKit
+
+class AddDishViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
